fbfc2e45636eb412f394a87be222527b3e8ac7fa
Dylan Theriot <dylantheriot@gmail.com>
2020-10-29 16:22:49 -0500

    more tests fb

--- app/main.py
+++ app/main.py
@@ -17,20 +17,20 @@ import json
 # load_dotenv()
 
 # load firebase
-# cred = credentials.Certificate({
-#   "type": os.environ['FIREBASE_TYPE'],
-#   "project_id": os.environ['FIREBASE_PROJECT_ID'],
-#   "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
-#   "private_key": json.loads(os.environ['FIREBASE_PRIVATE_KEY']),
-#   "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
-#   "client_id": os.environ['FIREBASE_CLIENT_ID'],
-#   "auth_uri": os.environ['FIREBASE_AUTH_URI'],
-#   "token_uri": os.environ['FIREBASE_TOKEN_URI'],
-#   "auth_provider_x509_cert_url": os.environ['FIREBASE_AUTH_PROVIDER_X509_CERT_URL'],
-#   "client_x509_cert_url": os.environ['FIREBASE_CLIENT_X509_CERT_URL']
-# })
-# firebase_admin.initialize_app(cred)
-# db = firestore.client()
+cred = credentials.Certificate({
+  "type": os.environ['FIREBASE_TYPE'],
+  "project_id": os.environ['FIREBASE_PROJECT_ID'],
+  "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
+  "private_key": json.loads(os.environ['FIREBASE_PRIVATE_KEY']),
+  "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
+  "client_id": os.environ['FIREBASE_CLIENT_ID'],
+  "auth_uri": os.environ['FIREBASE_AUTH_URI'],
+  "token_uri": os.environ['FIREBASE_TOKEN_URI'],
+  "auth_provider_x509_cert_url": os.environ['FIREBASE_AUTH_PROVIDER_X509_CERT_URL'],
+  "client_x509_cert_url": os.environ['FIREBASE_CLIENT_X509_CERT_URL']
+})
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # load spotify object
 # spotify = SpotifyAPI()
