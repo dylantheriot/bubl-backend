e4a18f89f53d6ac48e2fc77dd8b883c1649bdf15
Dylan Theriot <dylantheriot@gmail.com>
2020-10-29 15:55:46 -0500

    added wrong json

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ cred = credentials.Certificate({
   "type": os.environ['FIREBASE_TYPE'],
   "project_id": os.environ['FIREBASE_PROJECT_ID'],
   "private_key_id": os.environ['FIREBASE_PRIVATE_KEY_ID'],
-  "private_key": json.dumps(os.environ['FIREBASE_PRIVATE_KEY']),
+  "private_key": json.loads(os.environ['FIREBASE_PRIVATE_KEY']),
   "client_email": os.environ['FIREBASE_CLIENT_EMAIL'],
   "client_id": os.environ['FIREBASE_CLIENT_ID'],
   "auth_uri": os.environ['FIREBASE_AUTH_URI'],
