5198a8ef4c10e788bd15f7fc116ccfe4be422a26
Dylan Theriot <dylantheriot@gmail.com>
2020-10-29 16:24:40 -0500

    maybe finished??

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import datetime
 from urllib.parse import urlencode
 from flask import redirect
 # from dotenv import load_dotenv
-# from .spotifyApi import SpotifyAPI
+from .spotifyApi import SpotifyAPI
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -33,7 +33,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # load spotify object
-# spotify = SpotifyAPI()
+spotify = SpotifyAPI()
 
 # start flask
 app = Flask(__name__)
@@ -42,37 +42,37 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-# @app.route('/spotify/connect')
-# def spotify_connect():
-#   uuid = request.args.get('uuid')
-#   oauth_url = spotify.return_auth_url(uuid)
-#   return redirect(oauth_url)
+@app.route('/spotify/connect')
+def spotify_connect():
+  uuid = request.args.get('uuid')
+  oauth_url = spotify.return_auth_url(uuid)
+  return redirect(oauth_url)
 
-# @app.route('/callback')
-# def callback():
-#     code = request.args.get('code')
-#     uuid = request.args.get('state')
-#     res = spotify.request_refresh_token_from_auth_code(code)
-#     expires_in = datetime.datetime.now() + datetime.timedelta(seconds=res['expires_in'])
-#     doc_ref = db.collection(u'users').document(uuid)
-#     doc_ref.set({
-#         u'refresh_token': res['refresh_token'],
-#         u'access_token': res['access_token'],
-#         u'is_spotify_connected': True,
-#         u'expires_in': expires_in
-#     })
-#     return "<h1> Spotify connected successfully! You may now close this tab</h1>"
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    uuid = request.args.get('state')
+    res = spotify.request_refresh_token_from_auth_code(code)
+    expires_in = datetime.datetime.now() + datetime.timedelta(seconds=res['expires_in'])
+    doc_ref = db.collection(u'users').document(uuid)
+    doc_ref.set({
+        u'refresh_token': res['refresh_token'],
+        u'access_token': res['access_token'],
+        u'is_spotify_connected': True,
+        u'expires_in': expires_in
+    })
+    return "<h1> Spotify connected successfully! You may now close this tab</h1>"
 
-# def update_access_token(uuid):
-#     doc_ref = db.collection(u'users').document(uuid)
-#     user_data = doc_ref.get().to_dict()
-#     res = spotify.request_new_access_token(user_data['refresh_token'])
-#     expires_in = datetime.datetime.now() + datetime.timedelta(seconds=res['expires_in'])
-#     refresh_token = user_data['refresh_token']
-#     if 'refresh_token' in res.keys():
-#       refresh_token = res['refresh_token']
-#     doc_ref.update({
-#         u'access_token': res['access_token'],
-#         u'refresh_token': refresh_token,
-#         u'expires_in': expires_in,
-#     })
+def update_access_token(uuid):
+    doc_ref = db.collection(u'users').document(uuid)
+    user_data = doc_ref.get().to_dict()
+    res = spotify.request_new_access_token(user_data['refresh_token'])
+    expires_in = datetime.datetime.now() + datetime.timedelta(seconds=res['expires_in'])
+    refresh_token = user_data['refresh_token']
+    if 'refresh_token' in res.keys():
+      refresh_token = res['refresh_token']
+    doc_ref.update({
+        u'access_token': res['access_token'],
+        u'refresh_token': refresh_token,
+        u'expires_in': expires_in,
+    })
